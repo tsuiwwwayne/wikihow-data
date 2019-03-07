8a12cabd7dc19007aac90f88e914fe8eeb65748f
Wayne Tsui <tsuiwwwayne@hotmail.com>
2019-03-07 02:12:24 +0000

    Added optional script to split titles file into train, test and validation files, can use pre-split files provided to have consistent comparisons

---
+++ split.py
@@ -0,0 +1,20 @@
+import random
+
+NUM_TEST_LIMIT = 6000
+NUM_VAL_LIMIT = 6000
+
+with open('titles.txt', 'rb') as file_in:
+    titles = [(random.random(), line) for line in file_in]
+titles.sort()
+
+with open('all_train.txt', 'wb') as file_train, \
+     open('all_test.txt', 'wb') as file_test, \
+     open('all_val.txt', 'wb') as file_val:
+
+    for i, (_, line) in enumerate(titles):
+        if i < NUM_TEST_LIMIT:
+            file_test.write(line)
+        elif i < (NUM_TEST_LIMIT + NUM_VAL_LIMIT):
+            file_val.write(line)
+        else:
+            file_train.write(line)

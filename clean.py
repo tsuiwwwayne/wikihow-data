c8e2f6a47549faaaffdae89f8476978b919b2b86
Wayne Tsui <tsuiwwwayne@hotmail.com>
2019-03-11 15:12:54 +0000

    Decoupled sentences without whitespace separation

--- clean.py
+++ clean.py
@@ -1,8 +1,22 @@
 import pandas as pd
 
+def decouple_sentences(string):
+    l = []
+    i = 0
+    end_punc = {'.', '?', '!'}
+    while i < len(string):
+        c = string[i]
+        prev_c = None if i == 0 else string[i-1]
+        if c.isalpha() and c.isupper() and prev_c in end_punc:
+            l.append(' ')
+        l.append(c)
+        i += 1
+    return ''.join(l)
+
 def reformat(string):
     string = ' '.join(string.split())
     string = string.strip(' ;')
+    string = decouple_sentences(string)
     return string if any(c.isalpha() for c in string) else ''
 
 # Clean the WikiHowSep.csv dataset and output a cleaned copy
